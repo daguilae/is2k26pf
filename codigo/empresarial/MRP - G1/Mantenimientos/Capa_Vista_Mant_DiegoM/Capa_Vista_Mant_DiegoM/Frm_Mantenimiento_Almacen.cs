097ff949cb0391c05faa2e591e3841c13833c6f0
221369 - DIEGO ANDRE MONTERROSO RABARIQUE <dmonterrosor5@miumg.edu.gt>
2026-03-28 18:50:09 -0600

    Mantenimientos Tipo_Material y Alamacenes

---
+++ codigo/empresarial/MRP - G1/Mantenimientos/Capa_Vista_Mant_DiegoM/Capa_Vista_Mant_DiegoM/Frm_Mantenimiento_Almacen.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Vista_Mant_DiegoM
+{
+    public partial class Frm_Mantenimiento_Almacen : Form
+    {
+        public Frm_Mantenimiento_Almacen()
+        {
+            InitializeComponent();
+            //parametros para navegador
+            Capa_Controlador_Navegador.Cls_ConfiguracionDataGridView config = new Capa_Controlador_Navegador.Cls_ConfiguracionDataGridView
+            {
+                Ancho = 1100,
+                Alto = 200,
+                PosX = 10,
+                PosY = 300,
+                ColorFondo = Color.AliceBlue,
+                TipoScrollBars = ScrollBars.Both,
+                Nombre = "dgv_Almacen"
+            };
+
+            string[] columnas = {
+             "Tbl_Almacen",
+             "Pk_Id_Almacen",
+             "Nombre_Almacen",
+             "Ubicacion_Almacen",
+             "Tipo_Almacen"
+
+             };
+
+            string[] sEtiquetas = {
+             "ID Almacen",
+             "Nombre Almacen",
+             "Ubicacion",
+             "Tipo Almacen"
+            };
+
+
+
+            int id_aplicacion = 401;
+            int id_modulo = 7;
+
+            navegador1.IPkId_Aplicacion = id_aplicacion;
+            navegador1.IPkId_Modulo = id_modulo;
+            navegador1.configurarDataGridView(config);
+            navegador1.SNombreTabla = columnas[0];
+            navegador1.SAlias = columnas;
+            navegador1.SEtiquetas = sEtiquetas;
+            navegador1.mostrarDatos();
+        }
+    }
+}
