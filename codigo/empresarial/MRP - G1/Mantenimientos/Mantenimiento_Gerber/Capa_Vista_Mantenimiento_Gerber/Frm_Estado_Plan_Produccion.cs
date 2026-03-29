c511117d312f2dee16d27547dc6bf6aa8c88cf51
GERBER ALEXANDER ASTURIAS TEJAXÚN <gasturiast@miumg.edu.gt>
2026-03-29 00:22:30 -0600

    Mantenimiento Estado Plan de produccion

---
+++ codigo/empresarial/MRP - G1/Mantenimientos/Mantenimiento_Gerber/Capa_Vista_Mantenimiento_Gerber/Frm_Estado_Plan_Produccion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Vista_Mantenimiento_Gerber
+{
+    public partial class Frm_Estado_Plan_Produccion : Form
+    {
+        public Frm_Estado_Plan_Produccion()
+        {
+            InitializeComponent();
+            //parametros para navegador
+            Capa_Controlador_Navegador.Cls_ConfiguracionDataGridView config = new Capa_Controlador_Navegador.Cls_ConfiguracionDataGridView
+            {
+                Ancho = 1100,
+                Alto = 200,
+                PosX = 10,
+                PosY = 300,
+                ColorFondo = Color.AliceBlue,
+                TipoScrollBars = ScrollBars.Both,
+                Nombre = "dgv_empleados"
+            };
+
+            string[] columnas = {
+     "Tbl_Estado_Plan_Produccion",
+     "Pk_Id_Estado_Plan_Produccion",
+     "Nombre_Estado_Plan_Produccion",
+     "Descripcion_Estado_Plan_Produccion"
+
+ };
+
+            string[] sEtiquetas = {
+    "Id Estado Plan de Produccion",
+     "Nombre Estado Plan de Produccion",
+     "Descripcion Estado Plan de Produccion"
+ };
+
+
+
+            int id_aplicacion = 401;
+            int id_modulo = 7;
+
+            navegador1.IPkId_Aplicacion = id_aplicacion;
+            navegador1.IPkId_Modulo = id_modulo;
+            navegador1.configurarDataGridView(config);
+            navegador1.SNombreTabla = columnas[0];
+            navegador1.SAlias = columnas;
+            navegador1.SEtiquetas = sEtiquetas;
+            navegador1.mostrarDatos();
+        }
+    }
+}
