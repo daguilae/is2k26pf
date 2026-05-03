7b2dcd516619f4c3a7cc045c612b2098b921d2fc
221369 - DIEGO ANDRE MONTERROSO RABARIQUE <dmonterrosor5@miumg.edu.gt>
2026-05-03 12:26:59 -0600

    Filtro agregado del proceso 2

--- codigo/empresarial/MRP - G1/DLLS/MVC_Consultar_Receta/Receta_Mariale/Capa_Vista_CVRecetas/Frm_Encabezado_BOM.Designer.cs
+++ codigo/empresarial/MRP - G1/DLLS/MVC_Consultar_Receta/Receta_Mariale/Capa_Vista_CVRecetas/Frm_Encabezado_BOM.Designer.cs
@@ -33,8 +33,8 @@ namespace Capa_Vista_CVRecetas
             this.Dg_BOM = new System.Windows.Forms.DataGridView();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.Dtp_Hasta = new System.Windows.Forms.DateTimePicker();
+            this.Dtp_Desde = new System.Windows.Forms.DateTimePicker();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtID = new System.Windows.Forms.TextBox();
@@ -42,39 +42,38 @@ namespace Capa_Vista_CVRecetas
             this.Btn_crear = new System.Windows.Forms.Button();
             this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.Lbl_listado = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Cmb_Estado = new System.Windows.Forms.ComboBox();
+            this.Lbl_Estado = new System.Windows.Forms.Label();
+            this.Cmb_Id = new System.Windows.Forms.ComboBox();
+            this.Lbl_id = new System.Windows.Forms.Label();
+            this.Btn_Limpiar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Dg_BOM)).BeginInit();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.Highlight;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(-303, -39);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(-227, -32);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(1355, 12);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1016, 10);
             this.flowLayoutPanel1.TabIndex = 22;
             // 
             // Dg_BOM
             // 
             this.Dg_BOM.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Dg_BOM.Location = new System.Drawing.Point(23, 165);
-            this.Dg_BOM.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Dg_BOM.Location = new System.Drawing.Point(17, 134);
             this.Dg_BOM.Name = "Dg_BOM";
             this.Dg_BOM.RowHeadersWidth = 51;
-            this.Dg_BOM.Size = new System.Drawing.Size(1683, 481);
+            this.Dg_BOM.Size = new System.Drawing.Size(1262, 391);
             this.Dg_BOM.TabIndex = 21;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(1014, 106);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(895, 84);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(88, 29);
+            this.label5.Size = new System.Drawing.Size(70, 23);
             this.label5.TabIndex = 20;
             this.label5.Text = "Hasta:";
             // 
@@ -82,37 +81,33 @@ namespace Capa_Vista_CVRecetas
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(618, 107);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(592, 87);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(102, 29);
+            this.label4.Size = new System.Drawing.Size(81, 23);
             this.label4.TabIndex = 19;
             this.label4.Text = "Desde: ";
             // 
-            // dateTimePicker2
+            // Dtp_Hasta
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(1109, 111);
-            this.dateTimePicker2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(265, 22);
-            this.dateTimePicker2.TabIndex = 18;
+            this.Dtp_Hasta.Location = new System.Drawing.Point(967, 88);
+            this.Dtp_Hasta.Name = "Dtp_Hasta";
+            this.Dtp_Hasta.Size = new System.Drawing.Size(200, 20);
+            this.Dtp_Hasta.TabIndex = 18;
             // 
-            // dateTimePicker1
+            // Dtp_Desde
             // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(726, 111);
-            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(265, 22);
-            this.dateTimePicker1.TabIndex = 17;
+            this.Dtp_Desde.Location = new System.Drawing.Point(679, 88);
+            this.Dtp_Desde.Name = "Dtp_Desde";
+            this.Dtp_Desde.Size = new System.Drawing.Size(200, 20);
+            this.Dtp_Desde.TabIndex = 17;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(521, 106);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(519, 86);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 29);
+            this.label3.Size = new System.Drawing.Size(67, 23);
             this.label3.TabIndex = 16;
             this.label3.Text = "Fecha";
             // 
@@ -120,19 +115,17 @@ namespace Capa_Vista_CVRecetas
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(-117, 114);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(-88, 93);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 29);
+            this.label2.Size = new System.Drawing.Size(81, 23);
             this.label2.TabIndex = 14;
             this.label2.Text = "Estado:";
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(-301, 119);
-            this.txtID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtID.Location = new System.Drawing.Point(-226, 97);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(172, 22);
+            this.txtID.Size = new System.Drawing.Size(130, 20);
             this.txtID.TabIndex = 13;
             this.txtID.Text = "Buscar por ID";
             // 
@@ -140,20 +133,19 @@ namespace Capa_Vista_CVRecetas
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(-309, -79);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(-232, -64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(460, 38);
+            this.label1.Size = new System.Drawing.Size(367, 29);
             this.label1.TabIndex = 12;
             this.label1.Text = "Listado de Ordenes Recibidas";
             // 
             // Btn_crear
             // 
             this.Btn_crear.Font = new System.Drawing.Font("Rockwell", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Btn_crear.Location = new System.Drawing.Point(1390, 106);
-            this.Btn_crear.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Btn_crear.Location = new System.Drawing.Point(1177, 84);
+            this.Btn_crear.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Btn_crear.Name = "Btn_crear";
-            this.Btn_crear.Size = new System.Drawing.Size(187, 30);
+            this.Btn_crear.Size = new System.Drawing.Size(140, 24);
             this.Btn_crear.TabIndex = 26;
             this.Btn_crear.Text = "Crear Nueva Receta";
             this.Btn_crear.UseVisualStyleBackColor = true;
@@ -162,60 +154,79 @@ namespace Capa_Vista_CVRecetas
             // flowLayoutPanel2
             // 
             this.flowLayoutPanel2.BackColor = System.Drawing.SystemColors.Highlight;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(23, 50);
-            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(17, 41);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(1676, 12);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(1257, 10);
             this.flowLayoutPanel2.TabIndex = 25;
             // 
             // Lbl_listado
             // 
             this.Lbl_listado.AutoSize = true;
             this.Lbl_listado.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_listado.Location = new System.Drawing.Point(16, 11);
-            this.Lbl_listado.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Lbl_listado.Location = new System.Drawing.Point(12, 9);
             this.Lbl_listado.Name = "Lbl_listado";
-            this.Lbl_listado.Size = new System.Drawing.Size(423, 38);
+            this.Lbl_listado.Size = new System.Drawing.Size(340, 29);
             this.Lbl_listado.TabIndex = 24;
             this.Lbl_listado.Text = "Listado de Recetas Creadas";
             // 
-            // comboBox1
+            // Cmb_Estado
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(313, 110);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(177, 24);
-            this.comboBox1.TabIndex = 29;
+            this.Cmb_Estado.FormattingEnabled = true;
+            this.Cmb_Estado.Location = new System.Drawing.Point(370, 87);
+            this.Cmb_Estado.Name = "Cmb_Estado";
+            this.Cmb_Estado.Size = new System.Drawing.Size(134, 21);
+            this.Cmb_Estado.TabIndex = 29;
             // 
-            // label6
+            // Lbl_Estado
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(207, 105);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(101, 29);
-            this.label6.TabIndex = 28;
-            this.label6.Text = "Estado:";
+            this.Lbl_Estado.AutoSize = true;
+            this.Lbl_Estado.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_Estado.Location = new System.Drawing.Point(290, 83);
+            this.Lbl_Estado.Name = "Lbl_Estado";
+            this.Lbl_Estado.Size = new System.Drawing.Size(81, 23);
+            this.Lbl_Estado.TabIndex = 28;
+            this.Lbl_Estado.Text = "Estado:";
             // 
-            // textBox1
+            // Cmb_Id
             // 
-            this.textBox1.Location = new System.Drawing.Point(23, 110);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(172, 22);
-            this.textBox1.TabIndex = 27;
-            this.textBox1.Text = "Buscar por ID";
+            this.Cmb_Id.FormattingEnabled = true;
+            this.Cmb_Id.Location = new System.Drawing.Point(132, 89);
+            this.Cmb_Id.Name = "Cmb_Id";
+            this.Cmb_Id.Size = new System.Drawing.Size(134, 21);
+            this.Cmb_Id.TabIndex = 30;
+            // 
+            // Lbl_id
+            // 
+            this.Lbl_id.AutoSize = true;
+            this.Lbl_id.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lbl_id.Location = new System.Drawing.Point(6, 89);
+            this.Lbl_id.Name = "Lbl_id";
+            this.Lbl_id.Size = new System.Drawing.Size(100, 23);
+            this.Lbl_id.TabIndex = 31;
+            this.Lbl_id.Text = "Buscar Id";
+            // 
+            // Btn_Limpiar
+            // 
+            this.Btn_Limpiar.Image = global::Capa_Vista_CVRecetas.Properties.Resources.delete_remove_trash_icon_177304;
+            this.Btn_Limpiar.Location = new System.Drawing.Point(1285, 220);
+            this.Btn_Limpiar.Name = "Btn_Limpiar";
+            this.Btn_Limpiar.Size = new System.Drawing.Size(75, 59);
+            this.Btn_Limpiar.TabIndex = 32;
+            this.Btn_Limpiar.Text = "Limpiar";
+            this.Btn_Limpiar.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Limpiar.UseVisualStyleBackColor = true;
+            this.Btn_Limpiar.Click += new System.EventHandler(this.Btn_Limpiar_Click);
             // 
             // Frm_Encabezado_BOM
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1851, 703);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(1388, 571);
+            this.Controls.Add(this.Btn_Limpiar);
+            this.Controls.Add(this.Lbl_id);
+            this.Controls.Add(this.Cmb_Id);
+            this.Controls.Add(this.Cmb_Estado);
+            this.Controls.Add(this.Lbl_Estado);
             this.Controls.Add(this.Btn_crear);
             this.Controls.Add(this.flowLayoutPanel2);
             this.Controls.Add(this.Lbl_listado);
@@ -223,13 +234,12 @@ namespace Capa_Vista_CVRecetas
             this.Controls.Add(this.Dg_BOM);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.dateTimePicker2);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Dtp_Hasta);
+            this.Controls.Add(this.Dtp_Desde);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtID);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Frm_Encabezado_BOM";
             this.Text = "Encabezado_BOM";
             ((System.ComponentModel.ISupportInitialize)(this.Dg_BOM)).EndInit();
@@ -243,8 +253,8 @@ namespace Capa_Vista_CVRecetas
         private System.Windows.Forms.DataGridView Dg_BOM;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker Dtp_Hasta;
+        private System.Windows.Forms.DateTimePicker Dtp_Desde;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtID;
@@ -252,8 +262,10 @@ namespace Capa_Vista_CVRecetas
         private System.Windows.Forms.Button Btn_crear;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
         private System.Windows.Forms.Label Lbl_listado;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox Cmb_Estado;
+        private System.Windows.Forms.Label Lbl_Estado;
+        private System.Windows.Forms.ComboBox Cmb_Id;
+        private System.Windows.Forms.Label Lbl_id;
+        private System.Windows.Forms.Button Btn_Limpiar;
     }
 }
