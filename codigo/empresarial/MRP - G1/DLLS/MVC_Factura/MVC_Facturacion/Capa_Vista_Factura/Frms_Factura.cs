976c28def90fc90a44b953ec4baebb2fb326db70
VennusAmery <vennussalguero@gmail.com>
2026-05-12 18:55:26 -0600

    hola encabezado de facturacion

---
+++ codigo/empresarial/MRP - G1/DLLS/MVC_Factura/MVC_Facturacion/Capa_Vista_Factura/Frms_Factura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Vista_Factura
+{
+    public partial class Frms_Factura : Form
+    {
+        public Frms_Factura()
+        {
+            InitializeComponent();
+        }
+
+        private void Frms_Factura_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
