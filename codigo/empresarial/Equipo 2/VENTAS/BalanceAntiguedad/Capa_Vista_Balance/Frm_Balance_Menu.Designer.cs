11dca70f3924ceea4e69d4636d6d616ced35275a
BrandonHernandez2708 <157521682+BrandonHernandez2708@users.noreply.github.com>
2026-05-16 18:24:13 -0600

    Ayudas realizadas e implementada en balance de antiguedades

--- codigo/empresarial/Equipo 2/VENTAS/BalanceAntiguedad/Capa_Vista_Balance/Frm_Balance_Menu.Designer.cs
+++ codigo/empresarial/Equipo 2/VENTAS/BalanceAntiguedad/Capa_Vista_Balance/Frm_Balance_Menu.Designer.cs
@@ -38,41 +38,42 @@ namespace Capa_Vista_Balance
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.Btn_Reporte = new System.Windows.Forms.Button();
             this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.Btn_Ayudas = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // Dtp_Inicio
             // 
-            this.Dtp_Inicio.Location = new System.Drawing.Point(17, 78);
-            this.Dtp_Inicio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Dtp_Inicio.Location = new System.Drawing.Point(23, 96);
+            this.Dtp_Inicio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Dtp_Inicio.Name = "Dtp_Inicio";
-            this.Dtp_Inicio.Size = new System.Drawing.Size(188, 20);
+            this.Dtp_Inicio.Size = new System.Drawing.Size(249, 22);
             this.Dtp_Inicio.TabIndex = 0;
             // 
             // Dtp_Final
             // 
-            this.Dtp_Final.Location = new System.Drawing.Point(244, 78);
-            this.Dtp_Final.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Dtp_Final.Location = new System.Drawing.Point(325, 96);
+            this.Dtp_Final.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Dtp_Final.Name = "Dtp_Final";
-            this.Dtp_Final.Size = new System.Drawing.Size(188, 20);
+            this.Dtp_Final.Size = new System.Drawing.Size(249, 22);
             this.Dtp_Final.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(92, 209);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(123, 257);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.Size = new System.Drawing.Size(0, 17);
             this.label1.TabIndex = 2;
             // 
             // Lbl_Fecha_Inicio
             // 
             this.Lbl_Fecha_Inicio.AutoSize = true;
             this.Lbl_Fecha_Inicio.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Fecha_Inicio.Location = new System.Drawing.Point(54, 42);
+            this.Lbl_Fecha_Inicio.Location = new System.Drawing.Point(72, 52);
+            this.Lbl_Fecha_Inicio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Fecha_Inicio.Name = "Lbl_Fecha_Inicio";
-            this.Lbl_Fecha_Inicio.Size = new System.Drawing.Size(97, 16);
+            this.Lbl_Fecha_Inicio.Size = new System.Drawing.Size(132, 20);
             this.Lbl_Fecha_Inicio.TabIndex = 63;
             this.Lbl_Fecha_Inicio.Text = "Fecha de inicio ";
             // 
@@ -80,14 +81,16 @@ namespace Capa_Vista_Balance
             // 
             this.Lbl_Fecha_Final.AutoSize = true;
             this.Lbl_Fecha_Final.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Lbl_Fecha_Final.Location = new System.Drawing.Point(292, 42);
+            this.Lbl_Fecha_Final.Location = new System.Drawing.Point(389, 52);
+            this.Lbl_Fecha_Final.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Lbl_Fecha_Final.Name = "Lbl_Fecha_Final";
-            this.Lbl_Fecha_Final.Size = new System.Drawing.Size(73, 16);
+            this.Lbl_Fecha_Final.Size = new System.Drawing.Size(98, 20);
             this.Lbl_Fecha_Final.TabIndex = 64;
             this.Lbl_Fecha_Final.Text = "Fecha final ";
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.Btn_Ayudas);
             this.groupBox1.Controls.Add(this.Btn_Reporte);
             this.groupBox1.Controls.Add(this.Lbl_Fecha_Inicio);
             this.groupBox1.Controls.Add(this.Lbl_Fecha_Final);
@@ -95,10 +98,10 @@ namespace Capa_Vista_Balance
             this.groupBox1.Controls.Add(this.Dtp_Inicio);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupBox1.Size = new System.Drawing.Size(1119, 131);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(1492, 161);
             this.groupBox1.TabIndex = 65;
             this.groupBox1.TabStop = false;
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
@@ -107,9 +110,10 @@ namespace Capa_Vista_Balance
             // 
             this.Btn_Reporte.Font = new System.Drawing.Font("Rockwell", 9.75F);
             this.Btn_Reporte.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Reporte.Image")));
-            this.Btn_Reporte.Location = new System.Drawing.Point(457, 27);
+            this.Btn_Reporte.Location = new System.Drawing.Point(609, 33);
+            this.Btn_Reporte.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Btn_Reporte.Name = "Btn_Reporte";
-            this.Btn_Reporte.Size = new System.Drawing.Size(74, 83);
+            this.Btn_Reporte.Size = new System.Drawing.Size(99, 102);
             this.Btn_Reporte.TabIndex = 67;
             this.Btn_Reporte.Text = "Imprimir";
             this.Btn_Reporte.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -122,23 +126,37 @@ namespace Capa_Vista_Balance
             this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
             this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 131);
-            this.crystalReportViewer1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 161);
+            this.crystalReportViewer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.Size = new System.Drawing.Size(1119, 518);
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1492, 638);
             this.crystalReportViewer1.TabIndex = 67;
-            this.crystalReportViewer1.ToolPanelWidth = 367;
+            this.crystalReportViewer1.ToolPanelWidth = 489;
             this.crystalReportViewer1.Load += new System.EventHandler(this.crystalReportViewer1_Load);
+            // 
+            // Btn_Ayudas
+            // 
+            this.Btn_Ayudas.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_Ayudas.Image = ((System.Drawing.Image)(resources.GetObject("Btn_Ayudas.Image")));
+            this.Btn_Ayudas.Location = new System.Drawing.Point(735, 33);
+            this.Btn_Ayudas.Margin = new System.Windows.Forms.Padding(4);
+            this.Btn_Ayudas.Name = "Btn_Ayudas";
+            this.Btn_Ayudas.Size = new System.Drawing.Size(111, 98);
+            this.Btn_Ayudas.TabIndex = 133;
+            this.Btn_Ayudas.Text = "AYUDA";
+            this.Btn_Ayudas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.Btn_Ayudas.UseVisualStyleBackColor = true;
+            this.Btn_Ayudas.Click += new System.EventHandler(this.Btn_Ayudas_Click);
             // 
             // Frm_Balance_Menu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1119, 649);
+            this.ClientSize = new System.Drawing.Size(1492, 799);
             this.Controls.Add(this.crystalReportViewer1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Frm_Balance_Menu";
             this.Text = "Frm_Balance_Menu";
             this.groupBox1.ResumeLayout(false);
@@ -158,5 +176,6 @@ namespace Capa_Vista_Balance
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button Btn_Reporte;
         private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        private System.Windows.Forms.Button Btn_Ayudas;
     }
 }
